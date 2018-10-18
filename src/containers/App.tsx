29bf91deacac1a611fda28cd4ed82aa4458891ff
Dag Erik Løvgren <dagerikhl@gmail.com>
2018-10-18 19:10:48 +0200

    Remove dummy code in App

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,133 +1,31 @@
 import * as React from 'react';
-import { FormEvent } from 'react';
-import { connect } from 'react-redux';
-import { Link, Route, RouteComponentProps, Switch, withRouter } from 'react-router-dom';
-import { bindActionCreators, Dispatch } from 'redux';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 import { withAuthentication } from '../components/higher-order/withAuthentication';
-import { LogoutButton } from '../components/LogoutButton';
 import { Routes } from '../constants/routes';
-import { AuthUser } from '../interfaces/AuthUser';
-import { firebaseDatabase } from '../services/firebase';
-import { DummyAction, simpleAction } from '../store/actions/dummy.action';
-import { State } from '../store/states/state';
 import { HomePage } from './HomePage';
+import { HoursPage } from './HoursPage/HoursPage';
 import { LoginPage } from './LoginPage/LoginPage';
 
 import './App.css';
-import { HoursPage } from './HoursPage/HoursPage';
 
-// TODO Remove when actual routing is implemented
-const dummyComponent = (title: string) => () => <div>{title}</div>;
-
-interface OwnState {
-    dummyMessage: string;
-}
-
-interface StateProps {
-    authUser: AuthUser | null;
-    dummyMember: string;
-}
-
-const mapStateToProps = (state: State): StateProps => ({
-    authUser: state.auth.authUser,
-    dummyMember: state.dummy.dummyMember
-});
-
-interface DispatchProps {
-    simpleAction: (payload: string) => void;
-}
-
-const mapDispatchToProps = (dispatch: Dispatch<DummyAction>): DispatchProps => bindActionCreators({
-    simpleAction
-}, dispatch);
-
-type AppProps = StateProps & DispatchProps & RouteComponentProps;
-
-class AppComponent extends React.PureComponent<AppProps, OwnState> {
-    public state: OwnState = {
-        dummyMessage: ''
-    };
-
-    private dummyMessageInputElement: HTMLInputElement | null;
-
-    public componentWillMount() {
-        const dummyMessageRef = firebaseDatabase.ref('dummyMessage').orderByKey().limitToLast(100);
-        dummyMessageRef.on('child_added', (snapshot: any) => {
-            this.setState({ dummyMessage: snapshot.val() });
-        });
-    }
-
+class AppComponent extends React.PureComponent {
     public render() {
-        const { authUser } = this.props;
-
-        const navOptions = authUser
-            ? (<ul>
-                <li><Link to={Routes.HOME.path}>{Routes.HOME.name}</Link></li>
-                <li><Link to={Routes.HOURS.path}>{Routes.HOURS.name}</Link></li>
-                <li><Link to={Routes.SUMMARY.path}>{Routes.SUMMARY.name}</Link></li>
-                <li><LogoutButton/></li>
-            </ul>)
-            : (<ul>
-                <li><Link to={Routes.HOME.path}>{Routes.HOME.name}</Link></li>
-                <li><Link to={Routes.LOGIN.path}>{Routes.LOGIN.name}</Link></li>
-            </ul>);
-
         return (
             <div className="app">
                 <header className="app-header">
                     <h1 className="app-title">Welcome to React</h1>
                 </header>
-                <p className="a">
-                    To get started, edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <p>
-                    Test text to see that Travis has deployed.
-                </p>
-
-                {/* Router dummy */}
-                <nav>
-                    {navOptions}
-                </nav>
 
                 <Switch>
                     <Route exact path={Routes.LOGIN.path} component={LoginPage}/>
                     <Route exact path={Routes.HOME.path} component={HomePage}/>
                     <Route exact path={Routes.HOURS.path} component={HoursPage}/>
-                    <Route exact path={Routes.SUMMARY.path} render={dummyComponent(Routes.SUMMARY.name)}/>
+                    {/*<Route exact path={Routes.SUMMARY.path} component={SummaryPage}/>*/}
                 </Switch>
-
-                {/* Redux dummy */}
-                <button
-                    onClick={this.handleOnClick}
-                >
-                    Press to test
-                </button>
-
-                {/* Firebase dummy */}
-                <form onSubmit={this.addMessage}>
-                    <div><p>{this.state.dummyMessage}</p></div>
-                    <input type="text" ref={(element) => this.dummyMessageInputElement = element}/>
-                    <input type="submit"/>
-                </form>
             </div>
         );
     }
-
-    // Redux dummy function
-    private handleOnClick = () => {
-        this.props.simpleAction('a new dummy value');
-    };
-
-    // Firebase dummy function
-    private addMessage = (e: FormEvent) => {
-        e.preventDefault();
-
-        if (this.dummyMessageInputElement) {
-            firebaseDatabase.ref('dummyMessage').push(this.dummyMessageInputElement.value);
-            this.dummyMessageInputElement.value = '';
-        }
-    };
 }
 
-export const App = withRouter(connect(mapStateToProps, mapDispatchToProps)(withAuthentication(AppComponent)));
+export const App = withRouter(withAuthentication(AppComponent));
