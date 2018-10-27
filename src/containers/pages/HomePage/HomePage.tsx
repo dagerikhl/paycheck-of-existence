300174572a5eb51ecd1eaf8876ffd633ba969643
Dag Erik Løvgren <dagerikhl@gmail.com>
2018-10-27 18:37:27 +0200

    Create global base for titlepages

--- src/containers/pages/HomePage/HomePage.tsx
+++ src/containers/pages/HomePage/HomePage.tsx
@@ -6,7 +6,9 @@ class HomePageComponent extends React.PureComponent {
     public render() {
         return (
             <section className="home-page">
-                <h1>Home page</h1>
+                <h1 className="title">Home page</h1>
+
+                Content
             </section>
         );
     }
