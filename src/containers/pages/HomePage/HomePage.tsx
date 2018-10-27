a65a4f442c115fd3194a7ea683710da0d75e04c4
Dag Erik Løvgren <dagerikhl@gmail.com>
2018-10-27 18:43:14 +0200

    Implement and design homepage

--- src/containers/pages/HomePage/HomePage.tsx
+++ src/containers/pages/HomePage/HomePage.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 
+import logo from '../../../images/logo.svg';
+
 import './HomePage.css';
 
 class HomePageComponent extends React.PureComponent {
     public render() {
         return (
             <section className="home-page">
-                <h1 className="title">Home page</h1>
+                <h1 className="title">Home</h1>
 
-                Content
+                <img className="logo" src={logo} alt="Logo"/>
+
+                <div className="message">
+                    Welcome to Paycheck of Existence! If you are me, that is.
+                    <br/>
+                    <br/>
+                    If not, please go away. But know that you are loved.
+                </div>
             </section>
         );
     }
