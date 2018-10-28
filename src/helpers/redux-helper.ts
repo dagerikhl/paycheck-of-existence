ec1b47e9ef37847d073c1f391332a8e8e756c279
Dag Erik Løvgren <dagerikhl@gmail.com>
2018-10-28 21:39:52 +0100

    Refactor redux helper method signature for better codestyle

--- src/helpers/redux-helper.ts
+++ src/helpers/redux-helper.ts
@@ -1,6 +1,6 @@
 import { Action, ActionCreatorsMapObject, bindActionCreators, Dispatch } from 'redux';
 
-export const createDispatchToPropsFunction = <A extends Action, T extends ActionCreatorsMapObject<A>>
-(dispatchFunctions: T) => {
-    return (dispatch: Dispatch) => bindActionCreators(dispatchFunctions, dispatch);
-};
+export const createDispatchToPropsFunction =
+    <A extends Action, T extends ActionCreatorsMapObject<A>>(dispatchFunctions: T) => {
+        return (dispatch: Dispatch) => bindActionCreators(dispatchFunctions, dispatch);
+    };
