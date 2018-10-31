a50df22ffbd398beaf51b58e498db6410accc1ac
Dag Erik Løvgren <dagerikhl@gmail.com>
2018-10-31 22:00:47 +0100

    Remove functionality for updating All weeks in HoursPage
    Letting Redux functionality stick around for now. In case it becomes
    relevant in the future.

--- src/containers/pages/HoursPage/HoursPage.tsx
+++ src/containers/pages/HoursPage/HoursPage.tsx
@@ -7,47 +7,30 @@ import { withAuthorization } from '../../../components/higher-order/withAuthoriz
 import { Theme } from '../../../constants/enums/Theme';
 import { Weeks } from '../../../constants/interfaces/Weeks';
 import { createArrayFromRange } from '../../../helpers/number-helper';
-import { createDispatchToPropsFunction } from '../../../helpers/redux-helper';
-import { updateAllWeeksAction } from '../../../store/actions/hours.action';
 import { State } from '../../../store/states/state';
-import { DataControls } from './DataControls';
 import { WeekTable } from './WeekTable';
 
 import './HoursPage.css';
 
 interface OwnState {
     lastVisibleWeekNumber: number;
-    isDirty: boolean;
 }
 
 interface StateProps {
-    year: number;
     weeks: Weeks;
 }
 
 const mapStateToProps = (state: State): StateProps => ({
-    year: state.period.year,
     weeks: state.hours.weeks
 });
 
-interface DispatchProps {
-    updateAllWeeks: (weeks: Weeks) => void;
-}
-
-const mapDispatchToProps = createDispatchToPropsFunction({
-    updateAllWeeks: updateAllWeeksAction
-});
-
-type HoursPageProps = StateProps & DispatchProps;
-
-class HoursPageComponent extends React.PureComponent<HoursPageProps, OwnState> {
+class HoursPageComponent extends React.PureComponent<StateProps, OwnState> {
     private readonly currentWeek = moment().isoWeek();
 
     private weeks = createArrayFromRange(0, moment().isoWeek()).reverse();
 
     public state: OwnState = {
-        lastVisibleWeekNumber: Math.max(1, this.currentWeek - 1),
-        isDirty: false
+        lastVisibleWeekNumber: Math.max(1, this.currentWeek - 1)
     };
 
     public componentDidMount() {
@@ -55,24 +38,12 @@ class HoursPageComponent extends React.PureComponent<HoursPageProps, OwnState> {
     }
 
     public render() {
-        const { lastVisibleWeekNumber, isDirty } = this.state;
+        const { lastVisibleWeekNumber } = this.state;
 
         return (
             <section className="hours-page">
-                <DataControls
-                    className="controls"
-                    label="You have unsaved changes."
-                    saveLabel="Save all"
-                    cancelLabel="Discard all"
-                    onSave={this.saveAllChanges}
-                    onCancel={this.discardAllChanges}
-                    hide={!isDirty}
-                />
-
-                {/* TODO Remove */}
-                <Button onClick={this.toggleControls}>Toggle dirty</Button>
-
                 <div className="weeks">
+                    {/* TODO Should use weeks from Redux here */}
                     {this.weeks
                         .filter((weekNumber) => weekNumber >= lastVisibleWeekNumber)
                         .map((weekNumber, i) => <WeekTable
@@ -101,19 +72,6 @@ class HoursPageComponent extends React.PureComponent<HoursPageProps, OwnState> {
             lastVisibleWeekNumber: newShowAllWeeks ? Math.max(1, this.currentWeek - 1) : 1
         });
     };
-
-    // TODO Remove
-    private toggleControls = () => {
-        this.setState({ isDirty: !this.state.isDirty });
-    };
-
-    private saveAllChanges = () => {
-        // TODO Save changes to database
-    };
-
-    private discardAllChanges = () => {
-        // TODO Reset all weektables to data from database
-    };
 }
 
-export const HoursPage = withAuthorization(connect(mapStateToProps, mapDispatchToProps)(HoursPageComponent));
+export const HoursPage = withAuthorization(connect(mapStateToProps, undefined)(HoursPageComponent));
