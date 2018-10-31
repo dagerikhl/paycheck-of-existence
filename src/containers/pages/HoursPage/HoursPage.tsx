54a847b58c814446d59f500840dc71970d8e6fc2
Dag Erik Løvgren <dagerikhl@gmail.com>
2018-10-31 21:57:31 +0100

    Remove check for member-ordering tsling + hide some weeks initially

--- src/containers/pages/HoursPage/HoursPage.tsx
+++ src/containers/pages/HoursPage/HoursPage.tsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import { Button } from '../../../components/Button';
 import { withAuthorization } from '../../../components/higher-order/withAuthorization';
+import { Theme } from '../../../constants/enums/Theme';
 import { Weeks } from '../../../constants/interfaces/Weeks';
 import { createArrayFromRange } from '../../../helpers/number-helper';
 import { createDispatchToPropsFunction } from '../../../helpers/redux-helper';
@@ -15,15 +16,18 @@ import { WeekTable } from './WeekTable';
 import './HoursPage.css';
 
 interface OwnState {
+    lastVisibleWeekNumber: number;
     isDirty: boolean;
 }
 
 interface StateProps {
     year: number;
+    weeks: Weeks;
 }
 
 const mapStateToProps = (state: State): StateProps => ({
-    year: state.period.year
+    year: state.period.year,
+    weeks: state.hours.weeks
 });
 
 interface DispatchProps {
@@ -37,18 +41,21 @@ const mapDispatchToProps = createDispatchToPropsFunction({
 type HoursPageProps = StateProps & DispatchProps;
 
 class HoursPageComponent extends React.PureComponent<HoursPageProps, OwnState> {
-    public state: OwnState = {
-        isDirty: false
-    };
+    private readonly currentWeek = moment().isoWeek();
 
     private weeks = createArrayFromRange(0, moment().isoWeek()).reverse();
+
+    public state: OwnState = {
+        lastVisibleWeekNumber: Math.max(1, this.currentWeek - 1),
+        isDirty: false
+    };
 
     public componentDidMount() {
         // TODO Start loading documents from the database
     }
 
     public render() {
-        const { isDirty } = this.state;
+        const { lastVisibleWeekNumber, isDirty } = this.state;
 
         return (
             <section className="hours-page">
@@ -66,15 +73,34 @@ class HoursPageComponent extends React.PureComponent<HoursPageProps, OwnState> {
                 <Button onClick={this.toggleControls}>Toggle dirty</Button>
 
                 <div className="weeks">
-                    {this.weeks.map((weekNumber, i) => <WeekTable
-                        key={i}
-                        weekNumber={weekNumber}
-                        isCurrent={i === 0}
-                    />)}
+                    {this.weeks
+                        .filter((weekNumber) => weekNumber >= lastVisibleWeekNumber)
+                        .map((weekNumber, i) => <WeekTable
+                            key={i}
+                            weekNumber={weekNumber}
+                            isCurrent={i === 0}
+                        />)}
                 </div>
+
+                {this.currentWeek > 2 && <Button
+                    className="show-all-button"
+                    theme={Theme.ACCENT}
+                    onClick={this.toggleShowAllWeeks}
+                >
+                    {lastVisibleWeekNumber === 1 ? 'Hide' : 'Load more...'}
+                </Button>}
             </section>
         );
     }
+
+    private toggleShowAllWeeks = () => {
+        const { lastVisibleWeekNumber } = this.state;
+
+        const newShowAllWeeks = lastVisibleWeekNumber === 1;
+        this.setState({
+            lastVisibleWeekNumber: newShowAllWeeks ? Math.max(1, this.currentWeek - 1) : 1
+        });
+    };
 
     // TODO Remove
     private toggleControls = () => {
