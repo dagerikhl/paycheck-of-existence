3916684d7914e80a746797d005ac6ed8b0b35992
Dag Erik Løvgren <dagerikhl@gmail.com>
2018-11-04 18:05:01 +0100

    Add date helper for moment() related methods

---
+++ src/helpers/date-helper.ts
@@ -0,0 +1,9 @@
+import * as moment from 'moment';
+
+export const getNewestWeekNumberInYear = (year: number) => {
+    const now = moment();
+
+    return year === now.year()
+        ? now.isoWeek()
+        : now.year(year).isoWeeksInYear();
+};
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,4 @@
+export * from './date-helper';
 export * from './number-helper';
 export * from './random-helper';
 export * from './redux-helper';
