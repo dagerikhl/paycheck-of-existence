bccb02760776314cece4887a1dc4c5ad5a4aa377
Dag Erik Løvgren <dagerikhl@gmail.com>
2018-11-06 19:30:58 +0100

    Iterate on input validation with onChange

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -13,13 +13,59 @@ interface OwnProps {
 
 type InputProps = OwnProps & React.HTMLProps<HTMLInputElement>;
 
-const InputComponent: React.SFC<InputProps> = ({ className, theme, square, round, children, ...rest }) => (
-    <input
-        className={`${className} input ${theme || Theme.NEUTRAL} ${square ? 'square' : ''} ${round ? 'round' : ''}`}
-        {...rest}
-    >
-        {children}
-    </input>
-);
+class InputComponent extends React.PureComponent<InputProps> {
+    public render() {
+        const { className, theme, square, round, children, value, ...rest } = this.props;
+
+        const classNames = `${className
+            } input ${theme || Theme.NEUTRAL
+            } ${square ? 'square' : ''
+            } ${round ? 'round' : ''}`;
+
+        return (
+            <input
+                className={classNames}
+                value={value}
+                onChange={this.onChange}
+                // TODO Implement onBlur to ensure a correct input at all times
+                // onBlur={this.onBlur}
+                {...rest}
+            >
+                {children}
+            </input>
+        );
+    }
+
+    private onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const { value, onChange } = this.props;
+
+        if (!onChange) {
+            return;
+        }
+
+        const newValue = event.currentTarget.value;
+        if (newValue !== value && this.isValid(newValue)) {
+            onChange(event);
+        }
+    };
+
+    private isValid = (newValue: number | string) => {
+        const { min, max, maxLength } = this.props;
+
+        if (min && min < newValue) {
+            return min;
+        }
+
+        if (max && max > newValue) {
+            return max;
+        }
+
+        if (typeof newValue === 'string' && maxLength && maxLength > newValue.length) {
+            return newValue.substr(0, maxLength);
+        }
+
+        return newValue;
+    };
+}
 
 export const Input = InputComponent;
