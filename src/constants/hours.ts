6220ad75c297aa7d872ff352d07880122c49a1b2
Dag Erik Løvgren <dagerikhl@gmail.com>
2019-06-10 18:07:21 +0200

    refactor: remove dead code for old totals

--- src/constants/hours.ts
+++ src/constants/hours.ts
@@ -1,11 +1,3 @@
-export const WEEK_COLUMNS = {
-    headers: [undefined, 'Date', 'Hours: !OT', 'SS: !OT', 'Hours: OT', 'SS: OT', 'OverTime', 'Notes'],
-    properties: ['status', 'date', 'hours', 'ssNo', 'hoursO', 'ssO', 'overtime', undefined],
-    classNames: ['status', 'date', 'hours-no', 'ss-no', 'hours-o', 'ss-o', 'overtime', undefined]
-};
-
-export const WEEK_ROWS_CSS = [undefined, undefined, undefined, undefined, undefined, 'weekend', 'weekend'];
-
 export const HOUR_LIMITS = {
     maxHoursPerWeek: 40
 };
